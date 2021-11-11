7b15a3ee47dedec5f62ec960bb34e2a78e5f8b1c
Musfiq <mushfiqur15-10872@diu.edu.bd>
2021-11-11 19:16:27 +0600

    Done

---
+++ app/src/main/java/com/example/broadcastreceiver/ExampleBroadcastReceiver.java
@@ -0,0 +1,23 @@
+package com.example.broadcastreceiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.widget.Toast;
+
+public class ExampleBroadcastReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if(ConnectivityManager.CONNECTIVITY_ACTION.equals(intent.getAction())){
+            boolean noConnectivity = intent.getBooleanExtra(
+                    ConnectivityManager.EXTRA_NO_CONNECTIVITY, false
+            );
+            if(noConnectivity){
+                Toast.makeText(context, "Disconnected", Toast.LENGTH_SHORT).show();
+            }else {
+                Toast.makeText(context, "Connected", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
